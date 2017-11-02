f38d672e22241f2b0c51a811e822817ae91f3ca9
Matthew Robinson <robinson.me@gmail.com>
2017-11-02 21:22:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  var a = a
+  var b = b
+  return a + b
+}
+
+function subtract(a, b) {
+  var a = a
+  var b = b
+  return a - b
+}
+
+function multiply(a, b) {
+  var a = a
+  var b = b
+  return a * b
+}
+
+function divide(a, b) {
+  var a = a
+  var b = b
+  return a / b
+}
+
+function inc(n) {
+  
+}
+
+function dec(n) {
+  
+}
+
+function makeInt(n) {
+  
+}
